5743b1fcee5545cb3f9172e854266cbe17af6f90
RuneRaiders <hyeonseoban47@gmail.com>
2025-03-24 16:01:45 +0900

    1. 테스트 케이스에 대한 고찰

--- src/test/java/io/hhplus/tdd/PointTest.java
+++ src/test/java/io/hhplus/tdd/PointTest.java
@@ -1,6 +1,7 @@
 package io.hhplus.tdd;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PointTest {
@@ -12,12 +13,35 @@ public class PointTest {
     *  3) 테스트 작성을 통해 요구사항에 대해 명확히 이해한 상태로 코드를 설계한다.
     *
     * 1. 작성해야 할 테스트 코드 : 총 4가지 기본 기능 (포인트 조회, 포인트 충전/사용 내역 조회, 충전, 사용) 을 구현
-    *  1) 포인트 조회
-    *  2) 포인트 충전/사용 내역 조회
-    *  3) 충전
-    *  4) 사용
+    *   1) 포인트 조회 : point
+    *     - id가 비어 있으면 실패한다.
+    *     - id의 type이 long이 아닐 경우 실패한다.
+    *
+    *  2) 포인트 충전/사용 내역 조회 : history
+    *     - id가 비어 있으면 실패한다.
+    *     - id의 type이 long이 아닐 경우 실패한다.
+    *     * PointHistory를 최대 20개까지만 반환하는 것을 규칙으로 선정
+    *       - PointHistory가 0보다 작으면 실패한다.
+    *       - PointHistory가 20개보다 크면 실패한다.
+    *
+    *  3) 충전 : charge
+    *     - id가 비어 있으면 실패한다.
+    *     - id의 type이 long이 아닐 경우 실패한다.
+    *     - amount의 type이 long이 아닐 경우 실패한다.
+    *     - amount가 0보다 작으면 실패한다.
+    *     * 1회 충전 최대 금액을 1,000,000까지만 가능한 것으로 규칙을 선정
+    *         - amount가 1,000,000보다 크면 실패한다.
+    *
+    *  4) 사용 : use
+    *     - id가 비어 있으면 실패한다.
+    *     - id의 type이 long이 아닐 경우 실패한다.
+    *     - amount의 type이 long이 아닐 경우 실패한다.
+    *     - amount가 0보다 작으면 실패한다.
+    *     - 해당 id가 가지고 있는 amount보다 큰 값이 입력될 경우 실패한다.
     *
     * */
+
+    @DisplayName("")
     @Test
     public void FirstTest(){
         String str = "aaa";
