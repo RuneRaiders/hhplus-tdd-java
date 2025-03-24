7bf68ca7330e4944b094f4bb9685584057a95066
RuneRaiders <hyeonseoban47@gmail.com>
2025-03-25 08:15:42 +0900

    feat : record 내 기본 기능 작성

--- src/main/java/io/hhplus/tdd/point/PointHistory.java
+++ src/main/java/io/hhplus/tdd/point/PointHistory.java
@@ -1,5 +1,8 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
+import java.util.List;
+
 public record PointHistory(
         long id,
         long userId,
@@ -7,4 +10,14 @@ public record PointHistory(
         TransactionType type,
         long updateMillis
 ) {
+
+    // 포인트 충전/사용 내역 추가
+    public void addPointHistory(PointHistoryTable pointHistoryTable, long userId, long amount, TransactionType type, long updateMillis) {
+        pointHistoryTable.insert(userId, amount, type, updateMillis);
+    }
+
+    // 포인트 충전/사용 내역 조회
+    public List<PointHistory> selectPointHistoryByUserId(PointHistoryTable pointHistoryTable, long userId){
+        return pointHistoryTable.selectAllByUserId(userId);
+    }
 }
--- src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -1,5 +1,7 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.UserPointTable;
+
 public record UserPoint(
         long id,
         long point,
@@ -8,5 +10,23 @@ public record UserPoint(
 
     public static UserPoint empty(long id) {
         return new UserPoint(id, 0, System.currentTimeMillis());
+    }
+
+    // 포인트 조회
+    public long getPoint(UserPointTable userPointTable, long id) {
+        UserPoint userPoint = userPointTable.selectById(id);
+        return userPoint.point();
+    }
+
+    // 포인트 충전
+    public void chargePoint(UserPointTable userPointTable, long id, long point) {
+        long userPoint = this.getPoint(userPointTable, id);
+        userPointTable.insertOrUpdate(id, userPoint + point);
+    }
+
+    // 포인트 사용
+    public void usePoint(UserPointTable userPointTable, long id, long point) {
+        long userPoint = this.getPoint(userPointTable, id);
+        userPointTable.insertOrUpdate(id, userPoint - point);
     }
 }
