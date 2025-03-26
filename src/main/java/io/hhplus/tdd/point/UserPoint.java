ac2d65cb42624e7cd8e115094b30da3a195625a6
RuneRaiders <hyeonseoban47@gmail.com>
2025-03-27 01:17:25 +0900

    refactor : UserPoint 내 예외처리 간, 예외처리 클래스가 적용되도록 변경

--- src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -1,5 +1,10 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.exception.ExceededMaxPointException;
+import io.hhplus.tdd.exception.ExceededPerPointException;
+import io.hhplus.tdd.exception.InsufficientChargedPointException;
+import io.hhplus.tdd.exception.InvalidPointException;
+
 public record UserPoint(
         long id,
         long point,
@@ -15,15 +20,15 @@ public record UserPoint(
 
     // 책임 분리 : 충전
     public UserPoint charge(long point){
-        if (point <= 0) throw new RuntimeException("충전할 포인트는 0보다 커야 합니다.");
+        if (point <= 0) throw new InvalidPointException("충전할 포인트는 0보다 커야 합니다.");
 
         if(point > MAX_PER_POINT){
-            throw new RuntimeException("한 번에 충전할 수 있는 포인트는 최대 1,000포인트 입니다.");
+            throw new ExceededPerPointException("한 번에 충전할 수 있는 포인트는 최대 1,000포인트 입니다.");
         }
 
         long totalPoint = this.point + point;
         if(totalPoint > MAX_TOTAL_POINT){
-            throw new RuntimeException("충전할 수 있는 포인트는 최대 10,000포인트 입니다.");
+            throw new ExceededMaxPointException("충전할 수 있는 포인트는 최대 10,000포인트 입니다.");
         }
 
         return new UserPoint(this.id, totalPoint, System.currentTimeMillis());
@@ -31,15 +36,15 @@ public record UserPoint(
 
     // 책임 분리 : 사용
     public UserPoint use(long point){
-        if (point <= 0) throw new RuntimeException("사용할 포인트는 0보다 커야 합니다.");
+        if (point <= 0) throw new InvalidPointException("사용할 포인트는 0보다 커야 합니다.");
 
         if(this.point < point){
-            throw new RuntimeException("충전할 수 있는 포인트는 최대 1,000포인트 입니다.");
+            throw new ExceededMaxPointException("충전할 수 있는 포인트는 최대 1,000포인트 입니다.");
         }
 
         long totalPoint = this.point - point;
         if(totalPoint < 0L){
-            throw new RuntimeException("보유 중인 포인트보다 많은 포인트는 사용할 수 없습니다.");
+            throw new InsufficientChargedPointException("보유 중인 포인트보다 많은 포인트는 사용할 수 없습니다.");
         }
 
         return new UserPoint(this.id, totalPoint, System.currentTimeMillis());
