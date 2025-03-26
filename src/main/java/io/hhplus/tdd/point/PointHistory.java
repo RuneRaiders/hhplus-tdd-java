efbcc76880ea25f54213d31e23f6c01ee7525958
RuneRaiders <hyeonseoban47@gmail.com>
2025-03-26 23:37:55 +0900

    fix : PointHistory에 책임 분리

--- src/main/java/io/hhplus/tdd/point/PointHistory.java
+++ src/main/java/io/hhplus/tdd/point/PointHistory.java
@@ -7,5 +7,9 @@ public record PointHistory(
         TransactionType type,
         long updateMillis
 ) {
-
+    public PointHistory {
+        if(type == null){
+            throw new IllegalArgumentException("transactionType 값이 존재하지 않습니다.");
+        }
+    }
 }
