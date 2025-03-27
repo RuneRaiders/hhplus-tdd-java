c350969af0ba6d1989f30aa150ab991227c784af
RuneRaiders <hyeonseoban47@gmail.com>
2025-03-28 00:06:17 +0900

    fix : UserPoint 예외 처리 문구 수정

--- src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -19,35 +19,31 @@ public record UserPoint(
     }
 
     // 책임 분리 : 충전
-    public UserPoint charge(long point){
-        if (point <= 0) throw new InvalidPointException("충전할 포인트는 0보다 커야 합니다.");
+    public UserPoint charge(long amount){
+        if (amount <= 0) throw new InvalidPointException("충전할 포인트는 0보다 커야 합니다.");
 
-        if(point > MAX_PER_POINT){
+        if(amount > MAX_PER_POINT){
             throw new ExceededPerPointException("한 번에 충전할 수 있는 포인트는 최대 1,000포인트 입니다.");
         }
 
-        long totalPoint = this.point + point;
+        long totalPoint = point + amount;
         if(totalPoint > MAX_TOTAL_POINT){
             throw new ExceededMaxPointException("충전할 수 있는 포인트는 최대 10,000포인트 입니다.");
         }
 
-        return new UserPoint(this.id, totalPoint, System.currentTimeMillis());
+        return new UserPoint(id, totalPoint, System.currentTimeMillis());
     }
 
     // 책임 분리 : 사용
-    public UserPoint use(long point){
+    public UserPoint use(long amount){
         if (point <= 0) throw new InvalidPointException("사용할 포인트는 0보다 커야 합니다.");
 
-        if(this.point < point){
-            throw new ExceededMaxPointException("충전할 수 있는 포인트는 최대 1,000포인트 입니다.");
-        }
-
-        long totalPoint = this.point - point;
+        long totalPoint = point - amount;
         if(totalPoint < 0L){
             throw new InsufficientChargedPointException("보유 중인 포인트보다 많은 포인트는 사용할 수 없습니다.");
         }
 
-        return new UserPoint(this.id, totalPoint, System.currentTimeMillis());
+        return new UserPoint(id, totalPoint, System.currentTimeMillis());
     }
 
 }
